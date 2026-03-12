73ee1cd3cfcbf8e4795e5517d63230f70663a629
Sumit Kumar <tbeast935@gmail.com>
2026-03-12 22:46:31 +0530

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "sitebuilder-e81e5.firebaseapp.com",
+  projectId: "sitebuilder-e81e5",
+  storageBucket: "sitebuilder-e81e5.firebasestorage.app",
+  messagingSenderId: "159027743033",
+  appId: "1:159027743033:web:4d14310a9d4eb05946c0e8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
